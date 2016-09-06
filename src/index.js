675c3179be7e54520ee8b3039d3dfd849d5be845
Kamakura Masaya <kamakura_m@waku-2.com>
2016-09-07 00:47:14 +0900

    feat(all): init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+export default function validationMiddleware() {
+  return (next) => (action) => validation(action, next);
+
+  function validation(action, next) {
+    return action.meta && action.meta.validation ?
+      runValidation(action, next) :
+      next(action);
+  }
+
+  function runValidation(action, next) {
+    let validationMessages = [];
+    const keys = Object.keys(action.meta.validation);
+    keys.map((key) => {
+      action.meta.validation[key]
+        .filter((validate) => validate.func != undefined &&
+          typeof validate.func === 'function' && validate.msg != undefined)
+        .map((validate) => {
+          if (!validate.func(action.payload))
+            validationMessages[key] =
+              validationMessages[key] == undefined ? validate.msg : [validationMessages[key], validate.msg];
+        });
+    });
+
+		next(Object.assign({}, action, { payload: { data: action.payload, validation: validationMessages } }));
+  }
+}
